2bd243cbe108fd4873c314ee42e0ce1fa9639549
HolyFrancis <holyfrancisdushimegmail.com>
2024-05-05 02:25:15 +0200

    started unit test

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from app.serializers import UserSerializer
+
+
+class UserSerializerTest(TestCase):
+    def test_serializer_valid_data(self):
+        data = {"username": "jack", "email": "jack@gmail.com", "first_name": "jack", "last_name": "buntu"}
+        serializer = UserSerializer(data=data)
+        self.assertTrue(serializer.is_valid())
+    
